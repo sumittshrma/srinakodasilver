24fac87d4af8a5323ef295ee366946abb920adba
sumittshrma <ss7675088@gmail.com>
2026-08-16 22:36:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        antique: resolve(__dirname, 'antique.html'),
+        jewellery: resolve(__dirname, 'jewellery.html'),
+        store: resolve(__dirname, 'store.html'),
+        plain: resolve(__dirname, 'plain.html'),
+        goldPolished: resolve(__dirname, 'gold-polished.html'),
+        templejewellery: resolve(__dirname, 'templejewellery.html'),
+        woodenitems: resolve(__dirname, 'woodenitems.html'),
+      },
+    },
+  },
 })
